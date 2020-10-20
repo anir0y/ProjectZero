d5c494e5c239706fa2a1bfea80e00066528645e6
Animesh Roy <anir0y@MACHomes-MacBook-Pro.local>
2020-10-20 22:44:15 +0530

    meta added

--- header.php
+++ header.php
@@ -2,10 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Arihsti Lab</title>  
-    <meta name="description" content= "Arishti Lab is designed for new people who wanna learn WAPT. this specific lab is designed for practicals by Animesh Roy (@anir0y)" />
-    <meta name="robots" content= "index, follow">
-    <meta author= "Animesh Roy"
+    <!-- Primary Meta Tags -->
+    <title>Arihsti Lab</title>
+    <meta name="title" content="Arihsti Lab">
+    <meta name="description" content="Arishti Lab is designed for new people who wanna learn WAPT. this specific lab is designed for practicals by Animesh Roy (@anir0y)">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://lab.vlab.space/login.php">
+    <meta property="og:title" content="Arihsti Lab">
+    <meta property="og:description" content="Arishti Lab is designed for new people who wanna learn WAPT. this specific lab is designed for practicals by Animesh Roy (@anir0y)">
+    <meta property="og:image" content="/img/logo.png">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://lab.vlab.space/login.php">
+    <meta property="twitter:title" content="Arihsti Lab">
+    <meta property="twitter:description" content="Arishti Lab is designed for new people who wanna learn WAPT. this specific lab is designed for practicals by Animesh Roy (@anir0y)">
+    <meta property="twitter:image" content="/img/logo.png">   
+    <meta author= "Animesh Roy">
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180917471-1"></script>
     <script>
